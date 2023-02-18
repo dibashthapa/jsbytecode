ef0123430c9eecfaaa17af3138d36305028e4f3e
Dibash Thapa <dibashthapa55@gmail.com>
2023-02-18 21:41:57 +0545

    ignored target builds

--- src/tools/target/debug/deps/tools-0bb90e8cd4793ccd.d
+++
@@ -1,9 +0,0 @@
-/Users/dibashthapa/projects/crafting-intrepreter-rust/src/tools/target/debug/deps/tools-0bb90e8cd4793ccd.rmeta: src/main.rs src/ast.rs src/expr.rs src/ast_printer.rs src/token.rs
-
-/Users/dibashthapa/projects/crafting-intrepreter-rust/src/tools/target/debug/deps/tools-0bb90e8cd4793ccd.d: src/main.rs src/ast.rs src/expr.rs src/ast_printer.rs src/token.rs
-
-src/main.rs:
-src/ast.rs:
-src/expr.rs:
-src/ast_printer.rs:
-src/token.rs:
--- src/tools/target/debug/deps/tools-f30d7aed7fb3d7c9.d
+++
@@ -1,9 +0,0 @@
-/Users/dibashthapa/projects/crafting-intrepreter-rust/src/tools/target/debug/deps/tools-f30d7aed7fb3d7c9: src/main.rs src/ast.rs src/expr.rs src/ast_printer.rs src/token.rs
-
-/Users/dibashthapa/projects/crafting-intrepreter-rust/src/tools/target/debug/deps/tools-f30d7aed7fb3d7c9.d: src/main.rs src/ast.rs src/expr.rs src/ast_printer.rs src/token.rs
-
-src/main.rs:
-src/ast.rs:
-src/expr.rs:
-src/ast_printer.rs:
-src/token.rs:
